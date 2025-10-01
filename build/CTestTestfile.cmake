73514f04d8dbd4b0463d275a90dc8c250ad5ebac
DanielVT-ECE <danielmurray_@outlook.com>
2025-10-01 14:36:05 -0400

    Apply .gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/daniel/ece3514/a7vector2
-# Build directory: /home/daniel/ece3514/a7vector2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(my_test "/home/daniel/ece3514/a7vector2/build/my_test")
-set_tests_properties(my_test PROPERTIES  _BACKTRACE_TRIPLES "/home/daniel/ece3514/a7vector2/CMakeLists.txt;19;add_test;/home/daniel/ece3514/a7vector2/CMakeLists.txt;0;")
